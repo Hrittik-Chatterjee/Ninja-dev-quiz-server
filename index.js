e5d5c95d90be0a50f4a5c42439954967dd1ec5ed
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-07-09 13:12:42 +0600

    favicon error solving

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const favicon = require("serve-favicon");
+const path = require("path");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use(express.json());
 app.use(cors());
